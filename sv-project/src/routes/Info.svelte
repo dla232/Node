4ebbe80b62bcca0ae8b973402c4dcb494590f42e
dla232 <rapid@nexon.co.kr>
2022-07-13 15:03:35 +0900

    d

--- sv-project/src/routes/Info.svelte
+++ sv-project/src/routes/Info.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte";
+    import {img_url} from "../store";
     import Header from "../include/header.svelte";
     import Nav from "../include/nav.svelte";
     setTimeout(() => {
@@ -11,4 +12,9 @@
 <Nav />
 <div class="center_wrap" id="container">
     <h1 class="d1_title">Info</h1>
+    <p>설이 사진</p>
+    <div>
+        <img src="{$img_url}img01.jpg" alt="">
+        <img src="{$img_url}img02.jpg" alt="">
+    </div>
 </div>

2348d81eee1fe46261c5d18da5016f79b22289b2
dla232 <rapid@nexon.co.kr>
2023-05-17 10:24:18 +0900

    Aws 프론트 서버 구축 테스트

---
+++ Front_server/nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <img src="/char03.png" alt="Landscape picture"/>
+    </main>
+  )
+}

44a390b46378eee695613393f957fd16e2d042b8
dla232 <rapid@nexon.co.kr>
2023-05-18 15:39:12 +0900

    Test

--- Front_server/nextjs/src/app/page.tsx
+++ Front_server/nextjs/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <img src="/char03.png" alt="Landscape picture"/>
         <h1>Front Server AWS Test Page</h1>
-        <p>무중단 배포 완료</p>
+        <p>무중단 배포 완료22</p>
     </main>
   )
 }

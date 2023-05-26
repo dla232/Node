daa4b7057573c9873093c31c09663653a095d326
dla232 <rapid@nexon.co.kr>
2023-05-26 09:49:46 +0900

    기본 골격 작업

--- Front_server/nextjs/src/app/page.tsx
+++ Front_server/nextjs/src/app/page.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
+import Link from 'next/link';
+import HeaderComponent from '../component/header';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <HeaderComponent/>
         <img src="/char03.png" alt="Landscape picture"/>
         <h1>Front Server AWS Test Page</h1>
         <p>무중단 배포 완료2332</p>
         <p>커밋 테스트</p>
+        <Link href="/test">
+          About
+        </Link>
     </main>
   )
 }

261faa165d9be26e7eb006b1953ae415c0fb6c6d
dla232 <rapid@nexon.co.kr>
2023-09-06 13:23:56 +0900

    수정

--- Canvas/rapid_wv.js
+++ Canvas/rapid_wv.js
@@ -1,6 +1,7 @@
 function rapid_wv(param) {
     const canvas = param.container;
     const ctx = canvas.getContext("2d");
+    const ctx2 = canvas.getContext("2d");
     const backgroundImage = new Image();
     backgroundImage.src = param.backgroundImage;
     backgroundImage.onload = function() {
@@ -10,13 +11,17 @@ function rapid_wv(param) {
       const waveAmplitude = param.waveAmplitude;
       const waveFrequency = param.waveFrequency;
       const waveSpeed = param.waveSpeed;
-
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       for (let x = 0; x < canvas.width; x++) {
         const yOffset = canvas.height - backgroundImage.height + waveAmplitude + waveAmplitude * Math.sin(x * waveFrequency + waveSpeed * Date.now());
         const waveImageHeight = backgroundImage.height - Math.abs(yOffset - (canvas.height - backgroundImage.height));
+        ctx.globalAlpha = 1; // 0부터 1까지의 값으로 투명도 설정 가능
         ctx.drawImage(backgroundImage, x, 0, 1, waveImageHeight, x, yOffset, 1, waveImageHeight);
+        ctx.globalAlpha = 1;
+        // console.log(y2Offset);
       }
+      // ctx2.drawImage(image2,0,0);
+      // ctx.drawImage(backgroundImage,0,-700);
       requestAnimationFrame(drawWaveImage);
     }
   }
